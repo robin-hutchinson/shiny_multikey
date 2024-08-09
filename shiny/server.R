0af87b3e7bb30e6fb19c3364029f2cdfd243ee6b
Robin Hutchinson <robhut@ceh.ac.uk>
2024-08-09 17:46:51 +0100

    structure shiny

---
+++ shiny/server.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(DT)
+library(tidyverse)
+
+features <- read.csv("genera/features.csv")
+server <- function(input, output) {
+  
+   features_absent <- reactive({
+     
+     g <- features
+     
+     if(!is.null(input$vein_3_fork)) {
+       
+       g <- g %>%
+         filter(body_part == "Vein 3"& meaning != input$vein_3_fork) 
+       
+       
+     } 
+     
+     unique(g$genus)
+     
+   })
+
+  output$results <- renderDataTable(
+
+    features %>%
+      filter(!(genus %in% features_absent())) %>%
+      distinct(genus) %>%
+      arrange(genus)%>%
+      rename("Possible genera are" = genus) 
+
+   ,
+   rownames = FALSE)
+  
+  
+}
