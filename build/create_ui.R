2a6c685ae40e189187e11f3d805ccd510df2f1b9
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2025-09-19 14:08:48 +0100

    Update create_ui.R

--- build/create_ui.R
+++ build/create_ui.R
@@ -151,7 +151,11 @@ full_script <- c(full_script,
                  "),card(tags$h2('References'),",
                  references,
                  "), col_widths = c(4,8)),",
-                 "layout_columns(card(htmlOutput('results1')), 
-                                 card(htmlOutput('results2'))))")
+                 "layout_columns(card(tag$a('results1taxa'),
+                                      tag$a('results1matched')
+                                      tag$a('results1unmatched')), 
+                                 card(tag$a('results2taxa'),
+                                      tag$a('results2matched')
+                                      tag$a('results2unmatched'))))")
   
 write_lines(full_script, 'shiny/ui.R')
