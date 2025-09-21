863ec311db91b0d92af613cf6e27ce15ef83393d
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2025-09-21 13:25:48 +0100

    Update create_ui.R

--- build/create_ui.R
+++ build/create_ui.R
@@ -152,10 +152,10 @@ full_script <- c(full_script,
                  references,
                  "), col_widths = c(4,8)),",
                  "layout_columns(card(tags$span('The closest match is ',tags$em(verbatimTextOutput('results1taxa')), '.'),
-                                              tags$span('It matched on the following questions: ', verbatimTextOutput('results1matched'), '.'),
-                                              tags$span('It did not match on these questions:', verbatimTextOutput('results1unmatched'), '.')),
+                                              tags$span('It matched on the following questions: ', uiOutput('results1matched'), '.'),
+                                              tags$span('It did not match on these questions:', uiOutput('results1unmatched'), '.')),
                                   card(tags$span('The second closest match is ',tags$em(verbatimTextOutput('results2taxa')), '.'),
-                                              tags$span('It matched on the following questions: ', verbatimTextOutput('results2matched'), '.'),
-                                              tags$span('It did not match on these questions:', verbatimTextOutput('results2unmatched'), '.')))))")
+                                              tags$span('It matched on the following questions: ', uiOutput('results2matched'), '.'),
+                                              tags$span('It did not match on these questions:', uiOutput('results2unmatched'), '.')))))")
   
 write_lines(full_script, 'shiny/ui.R')
