c2cf32db164d162ac92b0679c20937b5b1fcbffb
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2025-09-21 13:50:12 +0100

    Update create_server.R

--- build/create_server.R
+++ build/create_server.R
@@ -190,20 +190,16 @@ full_script <- c(full_script,
     
 })
 
-unmatched2 <- reactiveVal(
 
-    results2 <- results() %>%
-      filter(row_number() == 2)
-
-    results2$unmatched_features
-
-)
 
   output$results2unmatched <- renderUI({
-    
-    tags$ul(
-      lapply(unmatched2(), tags$li)
-    )
+
+    results2 <- results() %>%
+      filter(row_number() == 2) %>%
+      mutate(unmatched_features = paste("<li>", unmatched_features, </li>))
+    unmatched_features <- paste("<ul>", results2$unmatched_features, "</ul>")
+
+    HTML(unmatched_features)
 
   })
 }
