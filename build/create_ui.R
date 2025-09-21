9f0586f9989d2c609c838866d8e50de1e4935dbe
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2025-09-21 15:44:11 +0100

    Update create_ui.R

--- build/create_ui.R
+++ build/create_ui.R
@@ -151,13 +151,11 @@ full_script <- c(full_script,
                  "),card(tags$h2('References'),",
                  references,
                  "), col_widths = c(4,8)),",
-                 "layout_columns(card(conditionalPanel(condition = 'nchar('results1matched') > 2',
-                                              tags$span('The closest match is ',uiOutput('results1taxa'), '.'),
-                                              tags$span('It matched on the following questions: ', uiOutput('results1matched'), '.')),
-                                      conditionalPanel(condition = 'nchar('results1unmatched') > 2',
-                                              tags$span('It did not match on these questions:', uiOutput('results1unmatched'), '.')),
+                 "layout_columns(card(tags$span('The closest match is ',uiOutput('results1taxa'), '.'),
+                                      tags$span('It matched on the following questions: ', uiOutput('results1matched'), '.'),
+                                      tags$span('It did not match on these questions:', uiOutput('results1unmatched'), '.')),
                                   card(tags$span('The second closest match is ',tags$em(uiOutput('results2taxa')), '.'),
-                                              tags$span('It matched on the following questions: ', uiOutput('results2matched'), '.'),
-                                              tags$span('It did not match on these questions:', uiOutput('results2unmatched'), '.')))))")
+                                       tags$span('It matched on the following questions: ', uiOutput('results2matched'), '.'),
+                                       tags$span('It did not match on these questions:', uiOutput('results2unmatched'), '.')))))")
   
 write_lines(full_script, 'shiny/ui.R')
