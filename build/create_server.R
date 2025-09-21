57589d15fbfda7c2ec81f3bb96ffa38aa453e010
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2025-09-21 16:26:09 +0100

    Update create_server.R

--- build/create_server.R
+++ build/create_server.R
@@ -195,9 +195,9 @@ full_script <- c(full_script,
   if(length(results2$matched_features) >= 1){
 
       results2 <- results2 %>%
-              mutate(matched_features = paste('tags$ul(tags$li(', matched_features, '))', sep = '')
+              mutate(matched_features = paste('<li>', matched_features, '</li>', sep = '')
 
-      HTML(results2$matched_features)
+      HTML('<ul>', results2$matched_features, '</ul>')
 
     } else { HTML('') }
     
@@ -212,9 +212,9 @@ full_script <- c(full_script,
     if(length(results2$unmatched_features) >= 1){
 
       results2 <- results2 %>%
-              mutate(unmatched_features = paste('tags$ul(tags$li(', unmatched_features, '))', sep = '')
+              mutate(unmatched_features = paste('<li>', unmatched_features, '</li>', sep = '')
 
-      HTML(results2$unmatched_features)
+      HTML('<ul>', results2$unmatched_features, '</ul>')
 
     } else { HTML('') }
     
